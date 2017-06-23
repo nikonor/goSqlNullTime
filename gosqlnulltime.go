84953f6978c34a2d593e43cbc1378b26049db229
Leonid Nikolaev <nikolaevlv@altarix.ru>
2017-06-23 16:57:59 +0300

    ready package

---
+++ gosqlnulltime.go
@@ -0,0 +1,24 @@
+package goSqlNullTime
+
+import (
+	"database/sql/driver"
+	"time"
+)
+
+type NullTime struct {
+	Time  time.Time
+	Valid bool
+}
+
+func (nt *NullTime) Scan(value interface{}) error {
+	nt.Time, nt.Valid = value.(time.Time)
+	return nil
+}
+
+func (nt NullTime) Value() (driver.Value, error) {
+	if !nt.Valid {
+		return nil, nil
+	}
+	return nt.Time, nil
+}
+
